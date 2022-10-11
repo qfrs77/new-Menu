4d3c9bd0e46325684bcf0dbce9f3793d1a90a25a
gec0001 <2363978426@qq.com>
2022-10-11 17:00:04 +0800

    添加了记录历史功能 by kaede

--- src/views/BrowHistoryView.vue
+++ src/views/BrowHistoryView.vue
@@ -1,12 +1,53 @@
 <template>
     <div class="history">
-        <h1>浏览历史</h1>
+        <ul>
+            <li v-for="item in history" :key="item.id" @click="addhistory(item.id,item.img,item.title,item.ftitle,item.onclick)">
+                <router-link :to="{path:'/detail',query:{id:item.id}}">
+                    <div class="list-detail-left">
+                        <img :src="[item.img]" alt />
+                    </div>
+                        <div class="list-detail-right">
+                        <h4>{{item.title}}</h4>
+                        <p>{{item.ftitle}}</p>
+                        <span>{{item.onclick}}收藏</span>
+                    </div>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default{
-    name:"BrowHistoryView"
+    name:"BrowHistoryView",
+    data() {
+    return {
+        history:[]
+
+    };
+    },
+    created() {
+        this.history = JSON.parse(localStorage.getItem("history")) || [];
+        console.log(this.history);
+    },
+    computed: {
+        
+    },
+    watch: {
+        
+    },
+    methods: {
+        addhistory(id,img,title,ftitle,onclick) {
+        this.history.push({
+        id:id,
+        img:img,
+        title:title,
+        ftitle:ftitle,
+        onclick:onclick,
+        })
+        localStorage.setItem("history", JSON.stringify(this.history));
+    }
+    }
 }
 
 </script>
@@ -14,5 +55,50 @@ export default{
 <style lang="scss">
 .history{
     margin-top: 70px;
+    width: 100%;
+    ul {
+      width: 100%;
+      li {
+        width: 100%;
+        a {
+          width: 100%;
+          display: flex;
+          justify-content: space-around;
+          margin-bottom: 10px;
+          color: #000;
+          text-decoration: none;
+          text-align: left;
+          .list-detail-left {
+            width: 40%;
+            img {
+              width: 100%;
+              height: 100px;
+              display: block;
+              border-radius: 5px;
+            }
+          }
+          .list-detail-right {
+            width: 60%;
+            position: relative;
+            margin-left: 10px;
+            h4 {
+              font-weight: bold;
+              text-align: left;
+              margin-bottom: 15px;
+            }
+            p {
+              overflow: hidden;
+              white-space: nowrap;
+              text-overflow: ellipsis;
+            }
+            span {
+              position: absolute;
+              bottom: 0;
+              left: 0;
+            }
+          }
+        }
+      }
+    }
 }
 </style>
